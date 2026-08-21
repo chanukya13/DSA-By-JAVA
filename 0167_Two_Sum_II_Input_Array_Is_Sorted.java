49749b1bee6a624143ab41259eb38486be0f22ca
chanukya13 <gudavallichanukya@gmail.com>
2026-08-21 12:56:10 +0530

    Create 0167_Two_Sum_II_Input_Array_Is_Sorted.java

---
+++ 0167_Two_Sum_II_Input_Array_Is_Sorted.java
@@ -0,0 +1,28 @@
+
+// Problem: Two Sum II - Input Array Is Sorted
+// LeetCode: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+// Difficulty: Medium
+// Topics: Array, Two Pointers, Binary Search
+
+Code:-
+
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target){
+        int left=0;
+        int right=numbers.length-1;
+
+        while(left< right){
+            int sum= numbers[left]+ numbers[right];
+                if(sum== target){
+                return new int[] {left + 1,right + 1};
+                } else if(sum < target){
+                    left++;
+                } else {
+                    right--;
+                }
+        }    
+        return new int[]{}; 
+    }
+}
+
