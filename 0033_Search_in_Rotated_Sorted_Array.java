672bb8058cad11a22bad0a7b1138534fc453546f
chanukya13 <gudavallichanukya@gmail.com>
2026-08-21 09:41:59 +0530

    Create 0033_Search_in_Rotated_Sorted_Array.java

---
+++ 0033_Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,33 @@
+Problem: Search in Rotated Sorted Array II
+LeetCode: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+Difficulty: Medium
+Topics: Array, Binary Search
+
+Code:-
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high= nums.length-1;
+        while(low<= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]== target) return mid;
+
+            if(nums[low] <= nums[mid]){
+                if( nums[low] <= target &&  target <= nums[mid]){
+                    high= mid-1;    
+                } else {
+                    low = mid+1;
+                }
+            } else {
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid+ 1;
+                } else {
+                    high= mid-1;
+                }
+            }
+        }
+        return -1;
+        
+    }
+}
