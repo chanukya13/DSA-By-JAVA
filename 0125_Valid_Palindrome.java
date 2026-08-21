8e9376353e5da0a25a141ad8df64c20d693bb753
chanukya13 <gudavallichanukya@gmail.com>
2026-08-21 12:51:19 +0530

    Create 0125_Valid_Palindrome.java

---
+++ 0125_Valid_Palindrome.java
@@ -0,0 +1,31 @@
+
+// Problem: Valid Palindrome
+// LeetCode: https://leetcode.com/problems/valid-palindrome/
+// Difficulty: Easy
+// Topics: Two Pointers, String
+
+
+Code:-
+
+class Solution {
+    public boolean isPalindrome(String s) {
+      int left= 0;
+      int right = s.length()-1;
+
+      while(left<right){
+        while (left < right && !Character.isLetterOrDigit(s.charAt(left))){
+           left++;
+        }   
+        while(left  < right && !Character.isLetterOrDigit(s.charAt(right))){
+           right--;
+        }
+
+        if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+            return false;
+        }
+        left++;
+        right--;          
+        }
+        return true;
+      }  
+    }
