54cc65340c01d026a0b1a5edb81287dd4db2ad8c
chanukya13 <gudavallichanukya@gmail.com>
2026-08-21 09:25:49 +0530

    Create 0027_Remove_Element.java

---
+++ 0027_Remove_Element.java
@@ -0,0 +1,21 @@
+ Problem: Remove Element
+ LeetCode: https://leetcode.com/problems/remove-element/
+ Difficulty: Easy
+ Topics: Array, Two Pointers
+
+Code:-
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i= 0;
+
+        for(int j=0;j<nums.length;j++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
+
